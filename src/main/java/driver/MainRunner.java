b0c9139ab1a51143517696318eee0e4731cb595d
mcanererdemm <mcanererdem@gmail.com>
2025-07-05 18:20:41 +0300

    Parallelized

--- src/main/java/driver/MainRunner.java
+++ src/main/java/driver/MainRunner.java
@@ -2,6 +2,7 @@ package driver;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"classpath:features"},
@@ -11,4 +12,9 @@ import io.cucumber.testng.CucumberOptions;
         dryRun = false, // True => Will not execute mapped step but will execute only unmapped steps
         plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
 public class MainRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
--- src/test/java/stepDefinitions/base/Hooks.java
+++ src/test/java/stepDefinitions/base/Hooks.java
@@ -1,8 +1,14 @@
 package stepDefinitions.base;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.sql.Timestamp;
 
 import static driver.DriverFactory.cleanUpDriver;
 import static driver.DriverFactory.getDriver;
@@ -13,6 +19,17 @@ public class Hooks {
     @Before
     public void setup() {
         getDriver();
+    }
+
+    @AfterStep
+    public void afterEachStep(Scenario scenario) {
+        if (scenario.isFailed()) {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            String timeInMilli = Long.toString(timestamp.getTime());
+
+            byte[] screenShot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenShot, "image/png", timeInMilli);
+        }
     }
 
     @After
